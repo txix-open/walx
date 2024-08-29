0696510862e214579b93147a888e2a59f671250c
Kirill Ryabov <sailor.kirill@gmail.com>
2024-08-29 18:20:39 +0300

    MSP-3988 add minIndexLagToLog option to replication server

--- replication/server_options.go
+++ replication/server_options.go
@@ -2,6 +2,7 @@ package replication
 
 import (
 	"crypto/tls"
+
 	"github.com/txix-open/walx"
 )
 
@@ -12,10 +13,11 @@ type WalOpener func() (*walx.Log, error)
 type serverOptions struct {
 	tls              *tls.Config
 	oldSegmentOpener WalOpener
+	minIndexLagToLog int64
 }
 
 func newServerOptions() *serverOptions {
-	return &serverOptions{}
+	return &serverOptions{minIndexLagToLog: 100_000}
 }
 
 func ServerTls(cfg *tls.Config) ServerOption {
@@ -27,5 +29,13 @@ func ServerTls(cfg *tls.Config) ServerOption {
 func ServerOldSegmentOpener(oldSegmentOpener WalOpener) ServerOption {
 	return func(opts *serverOptions) {
 		opts.oldSegmentOpener = oldSegmentOpener
+	}
+}
+
+func ServerMinIndexLagToLog(minIndexLagToLog int64) ServerOption {
+	return func(opts *serverOptions) {
+		if minIndexLagToLog > 0 {
+			opts.minIndexLagToLog = minIndexLagToLog
+		}
 	}
 }
